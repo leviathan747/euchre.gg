2f4009a8da2207ab4a53d8f70cc4b4732e132144
Levi Starrett <levi@roxsoftware.com>
2020-05-25 00:42:34 -0400

    update caches

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,14 +1,17 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache_v1';
-const RUNTIME = 'runtime_v7';
+const PRECACHE = 'precache_v2';
+const RUNTIME = 'runtime_v8';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '/euchre.gg', // Alias for index.html
+  '/euchre.gg/images/dealer.svg',
   '/euchre.gg/images/icon-192.png',
   '/euchre.gg/images/icon-512.png',
+  '/euchre.gg/images/play_alt-512.png',
+  '/euchre.gg/images/table.jpg',
   '/euchre.gg/index.html',
   '/euchre.gg/manifest.json',
   '/euchre.gg/service-worker.js'
